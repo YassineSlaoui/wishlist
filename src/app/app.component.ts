c01f0f4b1bf0c5a5a70d407278da27183c554f63
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-11-24 22:07:04 +0100

    Added ability to make new wishes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Wishlist';
   items: WishItem[] = [
-    new WishItem('Learn Angular Framework :)'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Do Good')
+    // new WishItem('Learn Angular Framework :)'),
+    // new WishItem('Get Coffee', true),
+    // new WishItem('Do Good')
   ];
+
+  newWishText = '';
+
+  title = 'Wishlist';
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
 
   toggleItem(item: WishItem) {
     console.log("Item \"" + item.wishText + "\" was previously " + item.isComplete);
