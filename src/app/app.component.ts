ac64ae43b59f2167f3a6b82d0372ae9428ee3166
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-11-25 12:21:47 +0100

    Added a Wish Filter, solved a "bug" my way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,23 +13,39 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('Learn Angular Framework :)'),
-    // new WishItem('Get Coffee', true),
-    // new WishItem('Do Good')
+    new WishItem('Learn Angular Framework :)'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Do Good')
   ];
 
-  newWishText = '';
+  listFilter: String = '0';
+
+  newWishText = ''; //This is litterally bound to the New Wish Text Input field using the [(ngModel)] Directive
 
   title = 'Wishlist';
+
+  visibleItems: WishItem[] = this.items;
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
+    this.filterChanged(this.listFilter);
+  }
+
+  filterChanged(value: any) {
+    console.log(value);
+    if (this.listFilter === '0')
+      this.visibleItems = this.items;
+    else if (this.listFilter === '1')
+      this.visibleItems = this.items.filter(item => !item.isComplete)
+    else
+      this.visibleItems = this.items.filter(item => item.isComplete)
   }
 
   toggleItem(item: WishItem) {
     console.log("Item \"" + item.wishText + "\" was previously " + item.isComplete);
     item.isComplete = !item.isComplete;
     console.log("Now is " + item.isComplete);
+    this.filterChanged(this.listFilter);
   }
 }
