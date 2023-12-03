7e86e7deea06d2529548c3da549cb866fd2f3a98
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2023-12-03 16:34:02 +0100

    Added a not found component and restyled the main app component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { WishComponent } from './wish/wish.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: WishComponent },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: NotFoundComponent }
 ];
